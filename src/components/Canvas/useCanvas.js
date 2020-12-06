8b0388f7ff0294e5e45048f7941eb0ee1a9b9d99
SoYoung Lee <soyoung.dev@gmail.com>
2020-12-06 17:28:54 +0900

    캔버스 - 크기 설정

--- src/components/Canvas/useCanvas.js
+++ src/components/Canvas/useCanvas.js
@@ -23,6 +23,25 @@ const useCanvas = (draw, dimension = '2d') => {
         };
     }, [draw, dimension]);
 
+    useEffect(() => {
+        const resize = () => {
+            const { current: canvas } = canvasRef;
+            const context = canvas.getContext(dimension);
+    
+            const { clientWidth: width, clientHeight: height } = document.body;
+            // 캔버스 더블 사이즈: 레티나 디스플레이 대응
+            canvas.width = width * 2;
+            canvas.height = height * 2;
+            context.scale(2, 2);
+        };
+        resize();
+
+        window.addEventListener('resize', resize);
+        return () => {
+            window.removeEventListener('resize', resize);
+        };
+    }, [dimension]);
+
     return canvasRef;
 };
 
