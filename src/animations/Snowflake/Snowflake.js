257b1984e57fca111c00962d19fc015c31ec2921
SoYoung Lee <soyoung.dev@gmail.com>
2020-12-06 18:41:42 +0900

    눈송이 애니메이션 - <Snowflake> 추가

---
+++ src/animations/Snowflake/Snowflake.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react';
+import Canvas from '../../components/Canvas';
+
+const Snowflake = () => {
+    let flakes = [];
+    const { clientWidth: width, clientHeight: height } = document.body;
+
+    const addFlake = () => {
+        const x = Math.ceil(Math.random() * width);
+        const r = Math.ceil(Math.random() * 4);
+        flakes.push({ x, y: 0, r });
+    };
+
+    const fallFlake = () => {
+        flakes.forEach(flake => { flake.y += flake.r / 2; });
+        // 화면 바깥으로 나간 flake 제거
+        flakes = flakes.filter(flake => flake.y < height);
+    };
+
+    const snow = () => {
+        fallFlake();
+        addFlake();
+    };
+
+    useEffect(() => {
+        const intervalId = setInterval(snow, 50);
+        return () => clearInterval(intervalId);
+    });
+
+
+    const draw = ctx => {
+        ctx.fillStyle = "#1F2124";
+        ctx.fillRect(0, 0, width, height);
+
+        flakes.forEach(flake => {
+            ctx.beginPath();
+            ctx.fillStyle = "rgba(255, 255, 255, 0.7)";
+            ctx.arc(flake.x, flake.y, flake.r / 2, 0, 2 * Math.PI);
+            ctx.fill();
+        });
+    };
+
+    return <Canvas draw={draw} />
+};
+
+export default Snowflake;
