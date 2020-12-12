1781b7c968344614ee97bf557a37b531d8359841
SoYoung Lee <soyoung.dev@gmail.com>
2020-12-12 09:02:38 +0900

    ♻️ 카드 컴포넌트로 분리

--- src/animations/TransparentCard/TransparentCard.js
+++ src/animations/TransparentCard/TransparentCard.js
@@ -1,35 +1,21 @@
 import React from "react";
+import Card from "./Card";
 import "./TransparentCard.scss";
-import chip from "../../assets/img/chip.png";
+
+const cardData = {
+    bankName: "Bank Name",
+    title: "Debit Card",
+    number: "1234 5678 9012 3456",
+    valid: "10/23",
+    name: "soyoung lee",
+    ccv: 132
+};
 
 function TransparentCard() {
     return (
         <section className="transparent-card">
-            <div className="card">
-                <div className="face front">
-                    <div className="title">
-                        <h3 className="debit">Debit Card</h3>
-                        <h3 className="bank">Bank Name</h3>
-                    </div>
-                    <img src={chip} className="chip" alt="card chip"></img>
-                    <h3 className="number">0123 4567 8901 2345</h3>
-                    <h5 className="valid">
-                        <span>Valid<br/>thru</span>
-                        <span>10/23</span>
-                    </h5>
-                    <h5 className="cardHolder">soyoung lee</h5>
-                </div>
-                <div className="face back">
-                    <div className="blackbar"></div>
-                    <div className="ccvtext">
-                        <h5>Authorized Signature-not valid unless signed</h5>
-                        <div className="whiteBar">
-                            <div className="ccv">123</div>
-                        </div>
-                        <p className="text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-                    </div>
-                </div>
-            </div>
+            <Card {...cardData} />
+            <Card {...cardData} />
         </section>
     );
 }
